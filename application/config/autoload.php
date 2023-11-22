540a740026a795dc1daf98919e1f9883f295d87e
My Do <dohungmy@gmail.com>
2023-11-22 15:34:52 +0700

    update migration

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session','pagination','form_validation','quick_filter');
+$autoload['libraries'] = array('database', 'session','pagination','form_validation','quick_filter','migration');
 
 
 /*
