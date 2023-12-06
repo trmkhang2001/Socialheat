e3457a796bfd04a9b4d57c7edd9a87ff38260ae0
minhkhang201 <trmkhang@gmail.com>
2023-12-06 10:49:45 +0700

    update

--- application/common/business/BusinessCrm.php
+++ application/common/business/BusinessCrm.php
@@ -3,7 +3,7 @@
 namespace app\common\business;
 
 use app\common\components\Upload;
-use app\models\CRM;
+use app\models\Crm;
 
 class BusinessCrm implements BusinessInterface
 {
@@ -26,11 +26,11 @@ class BusinessCrm implements BusinessInterface
     /**
      * get model instance
      * @param mixed $data
-     * @return CRM $model
+     * @return Crm $model
      */
     public static function getModel($data = array())
     {
-        $model = CRM::getInstance();
+        $model = Crm::getInstance();
         return $model;
     }
 
@@ -41,9 +41,9 @@ class BusinessCrm implements BusinessInterface
      */
     public function findOneCache($id)
     {
-        $query = CRM::getInstance()->findOne(array('id' => $id));
+        $query = Crm::getInstance()->findOne(array('id' => $id));
         $nameCache = 'findOne_' . $id;
-        return CRM::queryBuilder($nameCache, $query, TRUE);
+        return Crm::queryBuilder($nameCache, $query, TRUE);
     }
 
     /**
@@ -53,28 +53,28 @@ class BusinessCrm implements BusinessInterface
      */
     public function findOne($id)
     {
-        return CRM::getInstance()->findOne(array('id' => $id))->get()->row();
+        return Crm::getInstance()->findOne(array('id' => $id))->get()->row();
     }
 
     public function findByConditions($conditions = array(), $row = FALSE)
     {
-        $query = CRM::getInstance()->find()->where($conditions);
+        $query = Crm::getInstance()->find()->where($conditions);
         $nameCache = 'findByConditions' . http_build_query($conditions);
-        $res = CRM::queryBuilder($nameCache, $query, $row);
+        $res = Crm::queryBuilder($nameCache, $query, $row);
         return $res;
     }
 
 
     public function getByIdAsArray($id)
     {
-        return CRM::getInstance()->getByIdAsArray($id);
+        return Crm::getInstance()->getByIdAsArray($id);
     }
 
     public function findByMultipleId($ids = array())
     {
         $name = 'findByMultipleId' . http_build_query($ids);
-        $query = CRM::getInstance()->findByMultipleId($ids);
-        $dbDatas = CRM::queryBuilder($name, $query, FALSE);
+        $query = Crm::getInstance()->findByMultipleId($ids);
+        $dbDatas = Crm::queryBuilder($name, $query, FALSE);
         $ret = array();
         if ($dbDatas) {
             foreach ($dbDatas as $dbData) {
@@ -90,7 +90,7 @@ class BusinessCrm implements BusinessInterface
         if (($validation['validation'])) {
             return $validation;
         }
-        CRM::getInstance()->save($data);
+        Crm::getInstance()->save($data);
         $res = array(
             'success' => TRUE,
             'message' => 'Thêm số phone môi giới thành công'
@@ -106,7 +106,7 @@ class BusinessCrm implements BusinessInterface
                 return $validation;
             }
         }
-        CRM::getInstance()->update($id, $data);
+        Crm::getInstance()->update($id, $data);
         $res = array(
             'success' => TRUE,
             'message' => 'Cập nhật phone môi giới thành công'
@@ -116,14 +116,14 @@ class BusinessCrm implements BusinessInterface
 
     private function validateForm($data)
     {
-        $validation = CRM::getInstance()->validateForm($data);
+        $validation = Crm::getInstance()->validateForm($data);
         return $validation;
     }
 
     public function delete($id, $data)
     {
         //User::getInstance()->delete($id);
-        CRM::getInstance()->delete($id, $data);
+        Crm::getInstance()->delete($id, $data);
         $res = array(
             'success' => TRUE,
             'message' => 'Xóa thành công'
@@ -133,14 +133,14 @@ class BusinessCrm implements BusinessInterface
 
     public function getRange($conditions = array(), $offset = 0, $itemPerPage = 0, $orderBy = '')
     {
-        $dbObj = CRM::getInstance()->find();
+        $dbObj = Crm::getInstance()->find();
         if ($itemPerPage) {
             $dbObj->limit($itemPerPage);
         }
         if ($orderBy) {
             $dbObj->order_by($orderBy);
         }
-        $dbObj = CRM::getInstance()->getConditions($conditions, $dbObj);
+        $dbObj = Crm::getInstance()->getConditions($conditions, $dbObj);
         $dbObj->offset($offset);
         return $dbObj;
     }
@@ -149,36 +149,36 @@ class BusinessCrm implements BusinessInterface
     {
         $name = 'getRangeCache' . http_build_query($conditions) . $offset . $itemPerPage . $orderBy;
         $dbObj = $this->getRange($conditions, $offset, $itemPerPage, $orderBy);
-        return CRM::queryBuilder($name, $dbObj, FALSE);
+        return Crm::queryBuilder($name, $dbObj, FALSE);
     }
 
     public function getAllCache()
     {
         $name = 'getAll';
-        $dbObj = CRM::getInstance()->find();
-        return CRM::queryBuilder($name, $dbObj, TRUE);
+        $dbObj = Crm::getInstance()->find();
+        return Crm::queryBuilder($name, $dbObj, TRUE);
     }
 
     public function getCount($conditions = array())
     {
-        $dbObj = CRM::getInstance()->find();
-        $dbObj = CRM::getInstance()->getConditions($conditions, $dbObj);
+        $dbObj = Crm::getInstance()->find();
+        $dbObj = Crm::getInstance()->getConditions($conditions, $dbObj);
         return $dbObj->count_all_results();
     }
 
     public function findByConditionsCache($conditions)
     {
         $name = 'findByConditionsCache' . http_build_query($conditions);
-        $dbObj = CRM::findByConditions($conditions);
-        $res = CRM::queryBuilder($name, $dbObj, FALSE);
+        $dbObj = Crm::findByConditions($conditions);
+        $res = Crm::queryBuilder($name, $dbObj, FALSE);
         return $res;
     }
     public function getAllPhone()
     {
         $name = 'getAllPhone';
-        $dbObj = CRM::getInstance()->find();
+        $dbObj = Crm::getInstance()->find();
         $res = array();
-        $dbData = CRM::queryBuilder($name, $dbObj, FALSE);
+        $dbData = Crm::queryBuilder($name, $dbObj, FALSE);
         if ($dbData) {
             foreach ($dbData as $data) {
                 $res[] = $data->phone;
